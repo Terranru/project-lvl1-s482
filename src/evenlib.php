7ab5fe573e0463585ae97497c8fcc2693b8c1e7b
DenLok <denis@dlblog.ru>
2019-05-15 09:32:38 +0300

    evenlib

---
+++ src/evenlib.php
@@ -0,0 +1,12 @@
+<?php
+namespace BrainGames\Evenlib;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    line('Answer "yes" if number even otherwise answer "no".');
+    return;
+}
