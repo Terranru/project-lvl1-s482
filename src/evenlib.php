9c916501c948940921f6ce73518c6b09229415de
DenLok <denis@dlblog.ru>
2019-05-16 23:23:43 +0300

    function run

--- src/evenlib.php
+++ src/evenlib.php
@@ -8,5 +8,11 @@ function run()
 {
     line('Welcome to the Brain Game!');
     line('Answer "yes" if number even otherwise answer "no".');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    $random = random_int(0, 15);
+    line("Question: %s", $random);
+    $answer = prompt('Your answer');
+    
     return;
 }
