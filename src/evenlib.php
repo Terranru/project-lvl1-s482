461f742cdab9b76df9c49f70e397dc5c851b7963
DenLok <denis@dlblog.ru>
2019-05-17 14:28:44 +0300

    function even

--- src/evenlib.php
+++ src/evenlib.php
@@ -4,15 +4,28 @@ namespace BrainGames\Evenlib;
 use function \cli\line;
 use function \cli\prompt;
 
+function even($answer)
+{
+    if ($answer % 2 === 0) {
+        return 'yes';
+    }
+    else {
+        return 'no';
+    }
+}
+
 function run()
 {
     line('Welcome to the Brain Game!');
     line('Answer "yes" if number even otherwise answer "no".');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    $random = random_int(0, 15);
-    line("Question: %s", $random);
-    $answer = prompt('Your answer');
-    
+    for ($i=0; $i < 3; $i++) { 
+        $random = random_int(0, 15);
+        line("Question: %s", $random);
+        $answer = prompt('Your answer');
+    }
+
+
     return;
 }
